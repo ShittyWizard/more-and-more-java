c79a62c62bae2ae375cfb43c73777bffd1258e0c
ShittyWizard <anisimov.develop@gmail.com>
2018-08-31 23:46:44 +0300

    Add creating new directory with file inside and writing text there.

--- file-io/src/MainFileIO.java
+++ file-io/src/MainFileIO.java
@@ -1,3 +1,39 @@
-public class MainFileIO {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+public class MainFileIO {
+    public static void main(String[] args) {
+        try {
+            // Don't forget to delete existing directory for avoid AlreadyExistException!
+            Files.createDirectory(Paths.get("D:\\IDEA_projects\\more-and-more-java\\file-io\\src\\test-files"));
+            Files.createFile(Paths.get("D:\\IDEA_projects\\more-and-more-java\\file-io\\src\\test-files\\test_test_test"));
+        } catch (IOException e){
+            System.out.println(e.toString());
+        } finally {
+            System.out.println("Hello, this is finally");
+        }
+
+        try {
+            File file = new File("D:\\IDEA_projects\\more-and-more-java\\file-io\\src\\test-files\\test_test_test");
+            PrintWriter pw = new PrintWriter(file);
+            for (int i = 0; i < 10; i++) {
+                pw.write("test" + i + "\n");
+            }
+            pw.close();
+
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                System.out.println(sc.next());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        } finally {
+            System.out.println("Hello, this is finally x2");
+        }
+    }
 }
